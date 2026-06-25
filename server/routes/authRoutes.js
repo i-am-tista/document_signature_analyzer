0b3e302eb2546e2a01393cc04bea2cd77874f8ff
Riddhita Chakraborty <riddhita0508c@gmail.com>
2026-06-25 10:49:15 +0530

    Completed Day 2 JWT Authentication

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authMiddleware = require("../middleware/authMiddleware");
+
+const {
+    registerUser,
+    loginUser
+} = require("../controllers/authController");
+
+router.post("/register", registerUser);
+router.post("/login",loginUser);
+
+router.get(
+    "/profile",
+    authMiddleware,
+    (req, res) => {
+
+        res.json({
+            message: "Protected Route Accessed",
+            userId: req.user.id
+        });
+
+    }
+);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
-const express = require ("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
 app.get("/", (req, res) => {
     res.send("Backend Running");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
